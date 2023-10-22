6f93cc0bcffa978512dde11cc12b9600efcd259c
Mystic <215104920@qq.com>
2023-10-22 20:22:32 +0800

    feat: use lifespan instead of deprecated on_event

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -6,7 +8,15 @@ from db.base import create_db_and_tables, create_users
 from router.base import router
 from router.user_router import router as user_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):  # app is required, but not used now
+    create_db_and_tables()
+    create_users()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,13 +25,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.on_event("startup")
-async def on_startup():
-    create_db_and_tables()
-    create_users()
-
 
 app.include_router(router)
 app.include_router(user_router, prefix="/api")
